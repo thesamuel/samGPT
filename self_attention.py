cd32e93013ef1ecc7032b9f5cf3fac68adc743bd
Sam Gehman <sam.gehman@gmail.com>
2024-06-08 14:41:04 -0700

    Copy in single-head attention for reference

--- self_attention.py
+++ self_attention.py
@@ -5,6 +5,43 @@ from torch import nn
 from torch.nn import functional as F
 
 
+class SelfAttentionSingleHead(nn.Module):
+    """
+    Single-head self attention implementation
+
+    Included for reference, should always use SelfAttentionHead
+
+    """
+    def __init__(self, head_size: int, n_embd: int, block_size: int):
+        super().__init__()
+        self.head_size = head_size
+        self.n_embd = n_embd
+        self.key = nn.Linear(self.n_embd, self.head_size, bias=False)
+        self.query = nn.Linear(self.n_embd, self.head_size, bias=False)
+        self.value = nn.Linear(self.n_embd, self.head_size, bias=False)
+        self.register_buffer("tril", torch.tril(torch.ones(block_size, block_size)))
+
+    def forward(self, x):
+        B, T, C = x.shape
+
+        # C must equal the n_embd of the key, query, and value vectors
+        assert C == self.n_embd
+
+        k = self.key(x)  # (B, T, C)
+        q = self.query(x)  # (B, T, C)
+
+        # Compute attention scores ("affinities")
+        wei = (
+            q @ k.transpose(-2, -1) * self.head_size**-0.5
+        )  # (B, T, C) @ (B, C, T) -> (B, T, T)
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, -inf)  # B, T, T
+        wei = F.softmax(wei, dim=-1)  # (B, T, T)
+
+        # Perform weighted aggregation of the values
+        v = self.value(x)  # (B, T, head_size)
+        return wei @ v  # (B, T, T) @ (B, T, C) -> (B, T, head_size)
+
+
 class SelfAttentionHead(nn.Module):
     def __init__(self, n_embd: int, block_size: int, n_head: int = 1):
         super().__init__()
